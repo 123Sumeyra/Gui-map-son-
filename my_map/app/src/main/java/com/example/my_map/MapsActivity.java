51e1705c3a40a3cadcb81e14fd05dfa166180b83
123Sumeyra <ozugursumeyra@gmail.com>
2019-12-20 13:30:58 +0300

    initial condition

---
+++ my_map/app/src/main/java/com/example/my_map/MapsActivity.java
@@ -0,0 +1,184 @@
+package com.example.my_map;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+
+    private GoogleMap mMap;
+    LocationManager locationManager;
+    LocationListener locationListener = new LocationListener() {//kendi overridenı yaptı.
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+            /*
+            mMap.clear();
+            LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15)); //zoomluyoruz
+            //kameranın yerini değiştirmek için*/
+
+
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);//lokasyon izinlerini almamızı sağlar.
+
+            /*
+            if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //kontrol et bir izin varmı ACCESS_FINE_LOCATION kullanıcının yerine ulaşma izini var mı
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION},1);// bu requesti ayırabileceğimiz herhangi kod.
+            } else {//kullanıcı tarafından izin verilirse yapılması gerekenler.
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+            }
+            */
+        //ContexCompat yerine check self permission yapsaydık nasıl olurdu.sdk 23 ten az olonlarda hata olurdu.
+
+
+        if (Build.VERSION.SDK_INT >= 23) {//kullanıcını yerine ulaşma izinimiz var mı kontrol et.
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},1);
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+                Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);//sonbilinen lokasyonu al
+                System.out.println("lastLocation: " + lastLocation);
+                LatLng userLastLocation = new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude());//lokasyonu latlng a çevirdik
+                mMap.addMarker(new MarkerOptions().title("Your Location").position(userLastLocation));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLastLocation,15));
+            }
+        } else {//<23
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+            Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            System.out.println("lastLocation: " + lastLocation);
+            LatLng userLastLocation = new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude());
+            mMap.addMarker(new MarkerOptions().title("Your Location").position(userLastLocation));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLastLocation,15));
+        }
+
+
+        mMap.setOnMapLongClickListener(this);
+
+    }
+
+    @Override//kullanıcı izinleri verdiğinde yazılacak kod.
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //sonuçlar grandresult diye bir int array şeklinde geliyor.
+
+        if (grantResults.length > 0) {//bir sonuç geldiyse
+            if (requestCode == 1) {//requesti 1 verdik o mu geldi
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+        mMap.clear();
+
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        String address = "";//aldığımız adresi burda tutacağım.
+
+        try {
+            List<Address> addressList = geocoder.getFromLocation(latLng.latitude,latLng.longitude,1);
+            if (addressList != null && addressList.size() > 0) {
+                if (addressList.get(0).getThoroughfare() != null) {//cadde adı
+                    address += addressList.get(0).getThoroughfare();
+
+                    if (addressList.get(0).getSubThoroughfare() != null) {
+                        address += addressList.get(0).getSubThoroughfare();
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (address.matches("")) {
+            address = "No Address";
+        }
+
+        mMap.addMarker(new MarkerOptions().position(latLng).title(address));
+
+
+    }
+}
